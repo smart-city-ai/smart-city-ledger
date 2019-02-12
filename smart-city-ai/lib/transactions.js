e7da8681e98db9b85f1b683931b05b058f88809f
verititude <verititude@yummy.home>
2019-02-12 09:31:30 -0500

    done with verify

--- smart-city-ai/lib/transactions.js
+++ smart-city-ai/lib/transactions.js
@@ -67,6 +67,102 @@ async function VerifyDocument(verifyDocument) {
         });        
 }
 
+
+
+/**
+ * VerifyDocument using a given seal asset
+ * @param {ai.smartcity.VerifyDocumentBySeal} verifyDocument - verify document
+ * @transaction
+ */
+async function VerifyDocumentBySeal(verifyDocument) {
+    console.log("@debug: verify doc: " + verifyDocument);
+    console.log("@debug: verify office id: " +verifyDocument.office.officeId);
+    console.log("@debug: verify doc id: " +verifyDocument.document.docId);        
+    console.log("@debug: verify seal id: " +verifyDocument.seal.sealId);
+    
+    // check if office exists on the network
+    const officeRegistry = await  getParticipantRegistry('ai.smartcity.Office');
+    const officeExists = await officeRegistry.exists(verifyDocument.office.officeId);
+    if (officeExists == false) {
+        console.log('@debug: no such office ' + verifyDocument.document.office.officeId);        
+        throw new Error('Office does not exist - check Office id');
+    }
+    const office = await officeRegistry.get(verifyDocument.office.officeId);
+    console.log('@debug: office: ' + office);
+
+    const assetRegistry = await getAssetRegistry('ai.smartcity.Document')
+    const assetExist = await assetRegistry.exists(verifyDocument.document.docId);
+    if (assetExist == false) {
+        console.log('@debug: no such doc ' + verifyDocument.document.docId);
+        throw new Error('document does not exist');
+    }
+    var doc = await assetRegistry.get(verifyDocument.document.docId);
+    console.log('@debug: doc: ' + doc);
+
+    const sealRegistry = await getAssetRegistry('ai.smartcity.Seal')
+    const sealExist = await sealRegistry.exists(verifyDocument.seal.sealId);
+    if (sealExist == false) {
+        console.log('@debug: no such seal ' + verifyDocument.seal.sealId);
+        throw new Error('seal does not exist');
+    }
+    var seal = await sealRegistry.get(verifyDocument.seal.sealId);
+    console.log('@debug: seal: ' + seal);
+    
+    const url = office.imageServiceAddress;
+    const doc_image = doc.image;
+    const seal_image = seal.image;
+    
+    const data = {'truth_image': seal_image, "verify_image": doc_image};
+    const headers = {'Content-Type': 'application/json'};
+
+    console.log("@debug: image addr: " + url + ', seal name:' + seal.name);
+    const msg = await request.post({ uri: url, json: JSON.stringify(data) });
+    console.log("@debug: http response: " + JSON.stringify(msg));
+    const strMsg = JSON.stringify(msg);
+    const factory = getFactory();
+    var newStatus = factory.newConcept('ai.smartcity', 'sealStatus')
+    newStatus.sealName = seal.name;
+    newStatus.status = strMsg;
+    if (doc.sealStatus === undefined) {
+        doc.sealStatus = new Array();
+    }
+    doc.sealStatus.push(newStatus);
+    //get asset registry for Coins and Energy, and update on the ledger
+    return getAssetRegistry('ai.smartcity.Document')
+        .then(function (assetRegistry) {
+            return assetRegistry.updateAll([doc]);
+        });        
+}
+
+
+/**
+ * add a seal
+ * @param {ai.smartcity.AddSeal} addSeal - add a seal
+ * @transaction
+ */
+async function AddSeal(addSeal) {
+    const factory = getFactory();
+    const namespace = 'ai.smartcity';
+    
+    const bizRegistry = await  getParticipantRegistry(namespace + '.Business');
+    const bizExists = await bizRegistry.exists(addSeal.owner.businessId);
+    if (bizExists == false) {
+        console.log('@debug: no such biz ' + addSeal.owner.businessId);        
+        throw new Error('business does not exist - check business id');
+    }
+    console.log("@debug: to add seal");
+    return getAssetRegistry('ai.smartcity.Seal')
+        .then(function (assetRegistry) {
+            const sealId = addSeal.owner.businessId + "-" + addSeal.name;
+            var seal = factory.newResource(namespace,'Seal', sealId);
+            seal.name = addSeal.name;
+            seal.image = addSeal.image;
+            seal.owner = addSeal.owner;
+            console.log("@debug: adding seal " + seal);
+            return assetRegistry.add(seal);
+        });        
+}
+
 /**
  * setup a demo
  * @param {ai.smartcity.CreateDemo} createDemo - Create a Demo
--- test/cli.js
+++ test/cli.js
@@ -1,14 +1,16 @@
 // usage:
-// node ./cli.js --seal=seal.jpg --doc=doc.jpg --url=http://localhost:3000/api --setup=1
-// node ./cli.js --seal=seal.jpg --doc=doc.jpg --url=http://localhost:3000/api --setup=0
+// node ./cli.js --seal=seal.jpg --doc=doc.jpg --image_url=http://192.168.1.5:5000/api/v1/compare_images --blockchain_url=http://localhost:3000/api --setup=participant
+// node ./cli.js --seal=seal.jpg --doc=doc.jpg --image_url=http://192.168.1.5:5000/api/v1/compare_images --blockchain_url=http://localhost:3000/api --setup=doc
+// node ./cli.js --seal=seal.jpg --doc=doc.jpg --image_url=http://192.168.1.5:5000/api/v1/compare_images --blockchain_url=http://localhost:3000/api --setup=seal
+// node ./cli.js --seal=seal.jpg --doc=doc.jpg --image_url=http://192.168.1.5:5000/api/v1/compare_images --blockchain_url=http://localhost:3000/api --setup=verify
 
 'use strict';
 
 const args = require('yargs').argv;
 const seal = args.seal;
 const doc = args.doc;
-const url = args.url;
-const need_setup = args.setup;
+const url = args.blockchain_url;
+const image_url = args.image_url;
 
 const fs = require("fs");
 const seal_data = fs.readFileSync(seal).toString('base64')
@@ -19,34 +21,57 @@ const uuidv1 = require('uuid/v1');
 
 const headers = {'Content-Type': 'application/json'};
 
-const seal_name = 'test3';
-const seal_id = "seal3"; //uuidv1();
-const doc_id = "doc3"; //uuidv1();
-const office_id = "office3"; //uuidv1();
+const seal_name = 'my seal';
+const doc_id = "doc1"; 
+const office_id = "office1";
+const biz_id = "biz1";
+const seal_id = biz_id + '-' + seal_name; 
 
-function setup() {
-    const p_data = {
-        '$class': 'ai.smartcity.Seal',
-        'sealId':  seal_id,
-        'ownerID': uuidv1(),
-        'name': seal_name,
-        'image': seal_data
-    };
+function setup_participants() {
+    const o_data = {
+        '$class': 'ai.smartcity.Office',
+        'officeId': office_id,
+        'name': 'my office',
+        'imageServiceAddress': image_url
+    }
+
 
     axios({
         method: 'post',
-        url: url + '/Seal',
+        url: url + '/Office',
         headers: headers,
-        data: p_data
+        data: o_data
     })
         .then(msg => {
-            console.log('suceeded in creating seal');
+            console.log('suceeded in creating office');
         })
         .catch(function (error) {
-            console.log('failed in creating seal');
+            console.log('failed in creating office');
         });
+    
+    const b_data = {
+        '$class': 'ai.smartcity.Business',
+        'businessId': biz_id,
+        'name': 'my biz'
+    }
 
 
+    axios({
+        method: 'post',
+        url: url + '/Business',
+        headers: headers,
+        data: b_data
+    })
+        .then(msg => {
+            console.log('suceeded in creating biz');
+        })
+        .catch(function (error) {
+            console.log('failed in creating biz');
+        });
+    
+}
+
+function setup_doc() {
     const d_data = {
         '$class': 'ai.smartcity.Document',
         'docId': doc_id,
@@ -67,45 +92,43 @@ function setup() {
             console.log('failed in creating doc');
         });
 
-    const o_data = {
-        '$class': 'ai.smartcity.Office',
-        'officeId': office_id,
-        'name': 'my office',
-        'imageServiceAddress': 'http://192.168.1.5:5000/api/v1/compare'
-    }
+}
 
+function add_seal() {
+    const p_data = {
+        '$class': 'ai.smartcity.AddSeal',
+        'owner': 'resource:ai.smartcity.Business#'+biz_id,
+        'name': seal_name,
+        'image': seal_data
+    };
 
     axios({
         method: 'post',
-        url: url + '/Office',
+        url: url + '/AddSeal',
         headers: headers,
-        data: o_data
+        data: p_data
     })
         .then(msg => {
-            console.log('suceeded in creating office');
+            console.log('suceeded in creating seal');
         })
         .catch(function (error) {
-            console.log('failed in creating office');
-        });
+            console.log('failed in creating seal');
+        });    
 }
 
-if (need_setup == 1) {
-    setup();
-} else {
+function verify() {
     const v_data = {
-        '$class': 'ai.smartcity.VerifyDocument',
+        '$class': 'ai.smartcity.VerifyDocumentBySeal',
         'office': 'resource:ai.smartcity.Office#'+office_id,
         'document': 'resource:ai.smartcity.Document#'+doc_id,
-        'sealName': seal_name,
-        'transactionId': '',
-        'timestamp': '2020-01-11T00:00:00.00Z'
+        'seal': 'resource:ai.smartcity.Seal#'+seal_id,
     };
 
     console.log(v_data);
 
     axios({
         method: 'post',
-        url: url + '/VerifyDocument',
+        url: url + '/VerifyDocumentBySeal',
         headers: headers,
         data: v_data
     })
@@ -115,4 +138,21 @@ if (need_setup == 1) {
         .catch(function (error) {
             console.log('failed in transacting verify doc:' + error);
         });
+}
+
+switch(args.setup.toLowerCase()){
+case "participant":
+    setup_participants();
+    break;
+case "doc":
+    setup_doc();
+    break;
+case "seal":
+    add_seal();
+    break;
+case "verify":
+    verify();
+    break;
+default:
+    console.log("wrong args: " + args.setup);
 }
